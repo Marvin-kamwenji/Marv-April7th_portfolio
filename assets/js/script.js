ea52727f291b1a25819ab4b4ca63ef3fe7a6da12
emma-obelenge <obeleemmanuel@gmail.com>
2025-04-04 21:52:03 +0100

    implemented link functionality for projects section

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,3 +134,38 @@ const letsChatNavBtn = document.querySelector("#contact-nav-btn");
 letsChatNavBtn.addEventListener("click", () => {
     window.location.href = "#contact";
 })
+
+//handling projects click events
+const link = document.createElement("a");
+link.style.display = "none";
+link.target = "_blank";
+
+document.querySelector("#project-web-design").addEventListener("click", () => {
+    link.href = "https://emma-obelenge.github.io/emma-obelenge_portfolio/";
+    link.click();
+});
+
+document.querySelector("#project-ecommerce").addEventListener("click", () => {
+    link.href = "./coming-soon.html";
+    link.click();
+});
+
+document.querySelector("#project-graphics-design").addEventListener("click", () => {
+    link.href = "./coming-soon.html";
+    link.click();
+});
+
+document.querySelector("#project-it-support").addEventListener("click", () => {
+    link.href = "./coming-soon.html";
+    link.click();
+});
+
+document.querySelector("#project-calculator").addEventListener("click", () => {
+    link.href = "https://emma-obelenge.github.io/odin_calculator_project/";
+    link.click();
+});
+
+document.querySelector("#project-coding").addEventListener("click", () => {
+    link.href = "https://github.com/emma-obelenge?tab=repositories";
+    link.click();
+});
